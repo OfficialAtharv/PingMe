e586bf840e0f664c546264f88dfc63a0d4f91497
Atharv Kulkarni <atharvkulkarniofficial21@gmail.com>
2025-09-20 18:07:07 +0530

    Updated the chat page with right side profile section with status image and live status update with media

--- src/Componants/ChatBox/ChatBox.jsx
+++ src/Componants/ChatBox/ChatBox.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
-import './ChatBox.css'
-import assets from '../../assets/assets'
+import React from "react";
+import "./ChatBox.css";
+import assets from "../../assets/assets";
 const ChatBox = () => {
   return (
-    <div className='chatbox'>
+    <div className="chatbox">
       <div className="chatuser">
         <img src={assets.profile_img} alt="" />
-        <p>Atharv Kulkarni <img src={assets.green_dot} alt="" /></p>
+        <p>
+          Atharv Kulkarni <img className="dot" src={assets.green_dot} alt="" />
+        </p>
         <img src={assets.help_icon} alt="" />
       </div>
-      
+      <div className="chatmessage">
+        <div className="smassage">
+          <p className="message">Hello, This is a message Lorem ipsum dolor sit amet.</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+             <p>2:20PM</p>
+          </div>
+        </div>
+        <div className="smassage">
+         <img className="messageimg"src={assets.pic1} alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" />
+             <p>2:20PM</p>
+          </div>
+        </div>
+          <div className="rmassage">
+          <p className="message">Hello, This is a message Lorem ipsum dolor sit amet.</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+             <p>2:20PM</p>
+          </div>
+        </div>
+      </div>
+      <div className="chatinput">
+        <input type="text" placeholder="Send a message " />
+        <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatBox
+export default ChatBox;
--- src/Componants/RightSideBar/RightSideBar.jsx
+++ src/Componants/RightSideBar/RightSideBar.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import './RightSideBar.css'
+import React from "react";
+import "./RightSideBar.css";
+import assets from "../../assets/assets";
 const RightSideBar = () => {
   return (
-    <div>
-      
+    <div className="rs">
+      <div className="rsprofile">
+        <img src={assets.profile_img} alt="" />
+        <h3>
+          Atharv Kulkarni <img src={assets.green_dot} className="dot" alt="" />
+        </h3>
+        <p>Hey i am coding for the profile image !</p>
+      </div>
+      <hr />
+      <div className="rsmedia">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <div>
+        <button>Logout</button>
+      </div>
     </div>
   )
 }
 
-export default RightSideBar
+export default RightSideBar;
